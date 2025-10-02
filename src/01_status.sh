15f91b6da02ef8531f94ac97e4b4891bb033a14c
ajay-nagyal <anagyal@insilica.co>
2025-10-02 13:18:58 -0400

    rename downloads folder to download

--- src/01_status.sh
+++ src/01_status.sh
@@ -1,2 +1,2 @@
-mkdir -p downloads
-curl -o downloads/toxrefdb_status.html "https://clowder.edap-cluster.com/datasets/61147fefe4b0856fdc65639b#folderId=62c5cf1fe4b01d27e3b2d84a"
+mkdir -p download
+curl -o download/toxrefdb_status.html "https://clowder.edap-cluster.com/datasets/61147fefe4b0856fdc65639b#folderId=62c5cf1fe4b01d27e3b2d84a"
--- src/02_download.py
+++ src/02_download.py
@@ -3,7 +3,9 @@ import re
 import os
 
 # API endpoint to get all files in the dataset
-api_endpoint = "https://clowder.edap-cluster.com/api/datasets/61147fefe4b0856fdc65639b/files"
+api_endpoint = (
+    "https://clowder.edap-cluster.com/api/datasets/61147fefe4b0856fdc65639b/files"
+)
 
 # Make the API request
 response = requests.get(api_endpoint)
@@ -15,7 +17,7 @@ if response.status_code != 200:
 files = response.json()
 
 # Regex pattern to match toxrefdb...sql files
-pattern = r"mysql_toxrefdb.*\.sql"
+pattern = r"toxrefdb.*\.dump"
 
 # Filter for SQL files
 sql_files = [x for x in files if re.match(pattern, x.get("filename"))]
@@ -32,17 +34,17 @@ sql_file_id = sql_files[0].get("id")
 file_url = f"https://clowder.edap-cluster.com/api/files/{sql_file_id}"
 
 # Directory to save the file
-download_dir = "downloads"
+download_dir = "download"
 os.makedirs(download_dir, exist_ok=True)
 
 # Path to save the file
-download_path = os.path.join(download_dir, "mysql_toxrefdb.sql")
+download_path = os.path.join(download_dir, "toxrefdb.dump")
 
 # Stream the download
 with requests.get(file_url, stream=True) as r:
     r.raise_for_status()
-    with open(download_path, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=8192): 
+    with open(download_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
             f.write(chunk)
     print(f"Downloaded: {download_path}")
 
